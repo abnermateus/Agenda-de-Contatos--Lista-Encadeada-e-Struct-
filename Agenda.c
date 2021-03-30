d63b3147991db38ce3f032edab65cd4ffd7de8f1
abnermateus <75183543+abnermateus@users.noreply.github.com>
2021-03-29 23:51:55 -0300

    Commit

--- Agenda.c
+++ Agenda.c
@@ -21,19 +21,15 @@ Contato *criarContato()
     printf("Nome: ");
     fgets(contato->nome, TAM_STR - 1, stdin);
     contato->nome[strlen(contato->nome) - 1] = '\0';
-
     printf("Endereco: ");
     fgets(contato->endereco, TAM_STR - 1, stdin);
     contato->endereco[strlen(contato->endereco) - 1] = '\0';
-
     printf("Telefone residencial: ");
     fgets(contato->telefone_res, TAM_STR - 1, stdin);
     contato->telefone_res[strlen(contato->telefone_res) - 1] = '\0';
-
     printf("Telefone celular: ");
     fgets(contato->telefone_cel, TAM_STR - 1, stdin);
     contato->telefone_cel[strlen(contato->telefone_cel) - 1] = '\0';
-
     printf("Data de nascimento: ");
     fgets(contato->data_nascimento, TAM_STR - 1, stdin);
     contato->data_nascimento[strlen(contato->data_nascimento) - 1] = '\0';
@@ -62,7 +58,7 @@ Celula *criarCelula(ItemLista *item, Celula *prox)
 {
     Celula *celula = (Celula *)malloc(sizeof(Celula));
 
-    if (celula != NULL) 
+    if (celula != NULL)
     {
         celula->item = item;
         celula->prox = prox;
@@ -73,20 +69,28 @@ Celula *criarCelula(ItemLista *item, Celula *prox)
 typedef struct ListaEncadeada_T
 {
     Celula *inicio;
-    Celula *fim;    
-    int quantos;   
+    Celula *fim;
+    int quantos;
 } ListaEncadeada;
 
 ListaEncadeada *criarListaEncadeada()
 {
     ListaEncadeada *lista = (ListaEncadeada *)malloc(sizeof(ListaEncadeada));
 
-    if (lista != NULL) 
+    if (lista != NULL)
     {
         lista->inicio = lista->fim = NULL;
         lista->quantos = 0;
     }
     return lista;
+}
+
+int estah_vazia(ListaEncadeada *lista)
+{
+    if ((lista == NULL) || (lista->quantos == 0))
+        return 1;
+
+    return 0;
 }
 
 int inserirNoFim(ListaEncadeada *lista, ItemLista *item)
@@ -98,46 +102,17 @@ int inserirNoFim(ListaEncadeada *lista, ItemLista *item)
         Celula *novo = criarCelula(item, NULL);
 
         if (novo == NULL)
-            return 0; 
+            return 0;
 
         if (estah_vazia(lista) == 1)
             lista->inicio = lista->fim = novo;
         else
         {
-            lista->fim->prox = novo; 
+            lista->fim->prox = novo;
         }
         lista->quantos++;
         return 1;
     }
-}
-
-int inserirNoInicio(ListaEncadeada *lista, ItemLista *item)
-{
-    if (lista == NULL)
-        return 0;
-    else
-    {
-        Celula *novo = criarCelula(item, lista->inicio);
-
-        if (novo == NULL)
-            return 0; 
-
-        lista->inicio = novo;
-
-        if (estah_vazia(lista) == 1)
-            lista->fim = novo;
-
-        lista->quantos++;
-        return 1;
-    }
-}
-
-int estah_vazia(ListaEncadeada *lista)
-{
-    if ((lista == NULL) || (lista->quantos == 0))
-        return 1;
-
-    return 0;
 }
 
 int item_ehIgual(ItemLista *item1, ItemLista *item2)
@@ -148,57 +123,17 @@ int item_ehIgual(ItemLista *item1, ItemLista *item2)
     return 0;
 }
 
-ItemLista *excluirNoInicio(ListaEncadeada *lista)
-{
-    if ((lista == NULL) || (estah_vazia(lista) == 1))
-        return NULL;
-
-    Celula *aux = lista->inicio;
-    lista->inicio = aux->prox;
-    aux->prox = NULL;
-
-    ItemLista *temp = aux->item;
-    lista->quantos--;
-    free(aux);
-    return temp;
-}
-
-ItemLista *excluirNoFim(ListaEncadeada *lista)
-{
-    if ((lista == NULL) || (estah_vazia(lista) == 1))
-        return NULL;
-
-    Celula *aux = lista->inicio;
-    ItemLista *result = NULL;
-
-    if (tamanho(lista) == 1)
-        lista->inicio = lista->fim = NULL;
-    else
-    {
-        while (aux->prox != lista->fim)
-            aux = aux->prox;
-
-        lista->fim = aux;
-        aux = aux->prox;
-        lista->fim->prox = NULL;
-    }
-    result = aux->item;
-    free(aux);
-    lista->quantos--;
-    return result;
-}
-
 int excluir(ListaEncadeada *lista, ItemLista *item)
 {
-    if (lista == NULL) 
+    if (lista == NULL)
         return 0;
-    else if (estah_vazia(lista)) 
+    else if (estah_vazia(lista))
         return 0;
     else
     {
         Celula *aux = lista->inicio;
 
-        if (item_ehIgual(aux->item, item)) 
+        if (item_ehIgual(aux->item, item))
         {
             lista->inicio = aux->prox;
             aux->prox = NULL;
@@ -208,9 +143,9 @@ int excluir(ListaEncadeada *lista, ItemLista *item)
             return 1;
         }
 
-        else 
+        else
         {
-            while (aux->prox != NULL) 
+            while (aux->prox != NULL)
             {
                 if (item_ehIgual(aux->prox->item, item))
                 {
@@ -230,7 +165,7 @@ int excluir(ListaEncadeada *lista, ItemLista *item)
             }
         }
     }
-    return 0; 
+    return 0;
 }
 
 int tamanho(ListaEncadeada *lista)
@@ -254,8 +189,7 @@ ItemLista *pesquisarItem(ListaEncadeada *lista, texto_t nome)
             return aux->item;
 
         aux = aux->prox;
-    } 
-    while (aux != NULL);
+    } while (aux != NULL);
 
     return NULL;
 }
@@ -273,12 +207,58 @@ ItemLista **obterArray(ListaEncadeada *lista)
     return array;
 }
 
+void alterar(ListaEncadeada *lista, texto_t nome)
+{
+    printf("Informe o nome do contato a ser alterado: ");
+    fflush(stdin);
+    fgets(nome, TAM_STR - 1, stdin);
+
+    nome[strlen(nome) - 1] = '\0';
+    ItemLista *item = pesquisarItem(lista, nome);
+
+    if (item != NULL)
+    {
+        printf("Nome: ");
+        fgets(item->nome, TAM_STR - 1, stdin);
+        item->nome[strlen(item->nome) - 1] = '\0';
+
+        printf("Endereco: ");
+        fgets(item->endereco, TAM_STR - 1, stdin);
+        item->endereco[strlen(item->endereco) - 1] = '\0';
+
+        printf("Telefone residencial: ");
+        fgets(item->telefone_res, TAM_STR - 1, stdin);
+        item->telefone_res[strlen(item->telefone_res) - 1] = '\0';
+
+        printf("Telefone celular: ");
+        fgets(item->telefone_cel, TAM_STR - 1, stdin);
+        item->telefone_cel[strlen(item->telefone_cel) - 1] = '\0';
+
+        printf("Data de nascimento: ");
+        fgets(item->data_nascimento, TAM_STR - 1, stdin);
+        item->data_nascimento[strlen(item->data_nascimento) - 1] = '\0';
+    }
+    else
+        printf("O contato nao existe!\n");
+}
+
+void salvarContato(Contato *contato)
+{
+    FILE *arq = fopen("contatos.txt", "a");
+    fprintf(arq, "Nome: %s", contato->nome);
+    fprintf(arq, "Endereco: %s", contato->endereco);
+    fprintf(arq, "Telefone Residencial: %s", contato->telefone_res);
+    fprintf(arq, "Telefone Celular: %s", contato->telefone_cel);
+    fprintf(arq, "Data de Nascimento: %s", contato->data_nascimento);
+    printf(arq, "\n");
+}
+
 int menu()
 {
     int opcao;
 
     printf("\n======== ESTA E SUA AGENDA DE CONTATOS! ========\n");
-    printf("\nO que voce gostaria de fazer?\n"); 
+    printf("\nO que voce gostaria de fazer?\n");
     printf("\n1. Adicionar\n");
     printf("2. Remover\n");
     printf("3. Pesquisar\n");
@@ -306,9 +286,18 @@ int main()
 
         switch (opcao)
         {
-        case 1: printf("### ADICIONAR ####\n"); contato = criarContato(); inserirNoFim(lista, contato); break;
+        case 1:
+            printf("### ADICIONAR ####\n");
+            contato = criarContato();
+            inserirNoFim(lista, contato);
+            break;
 
-        case 2: printf("### REMOVER ####\n"); printf("Informe o nome: "); fgets(nome, TAM_STR - 1, stdin); nome[strlen(nome) - 1] = '\0';   contato = pesquisarItem(lista, nome);
+        case 2:
+            printf("### REMOVER ####\n");
+            printf("Informe o nome: ");
+            fgets(nome, TAM_STR - 1, stdin);
+            nome[strlen(nome) - 1] = '\0';
+            contato = pesquisarItem(lista, nome);
             if (contato != NULL)
             {
                 excluir(lista, contato);
@@ -319,49 +308,25 @@ int main()
                 printf("Excluir::Nenhum contato com o nome '%s'\n\n", nome);
             break;
 
-        case 3: printf("### PESQUISAR ####\n");printf("Informe o nome: "); fgets(nome, TAM_STR - 1, stdin); nome[strlen(nome) - 1] = '\0'; contato = pesquisarItem(lista, nome);
+        case 3:
+            printf("### PESQUISAR ####\n");
+            printf("Informe o nome: ");
+            fgets(nome, TAM_STR - 1, stdin);
+            nome[strlen(nome) - 1] = '\0';
+            contato = pesquisarItem(lista, nome);
             if (contato != NULL)
                 mostrarContato(contato);
             else
                 printf("Pesquisar::Nenhum contato com o nome '%s'\n\n", nome);
             break;
 
-        case 4: 
-            printf("### ALTERAR ####\n");
-            printf("Informe o nome: ");
-            fgets(nome, TAM_STR - 1, stdin);
-            nome[strlen(nome) - 1] = '\0';
-            texto_t nome2;
-            texto_t endereco2;
-            texto_t telefone_res2;
-            texto_t telefone_cel2;
-            texto_t data_nascimento2;
-            ItemLista *item = pesquisarItem(lista,nome);
-            printf("Digite o nome:\n");
-            fgets(nome2, TAM_STR - 1, stdin);
-            nome[strlen(nome) - 1] = '\0';
-            printf("digite o endereco:\n");
-            fgets(endereco2, TAM_STR - 1, stdin);
-            nome[strlen(nome) - 1] = '\0';
-            printf("digite o telefone residencial:\n");
-            fgets(telefone_res2, TAM_STR - 1, stdin);
-            nome[strlen(nome) - 1] = '\0';
-            printf("digite o telefone celular:\n");
-                fgets(telefone_cel2, TAM_STR - 1, stdin);
-                nome[strlen(nome) - 1] = '\0';
-                printf("digite o data de nascimento:\n");
-                fgets(data_nascimento2, TAM_STR - 1, stdin);
-                nome[strlen(nome) - 1] = '\0';
-                strcpy(item->nome,nome2);
-                strcpy(item->endereco,endereco2);
-                strcpy(item->telefone_res,telefone_res2);
-                strcpy(item->telefone_cel,telefone_cel2);
-                strcpy(item->data_nascimento,data_nascimento2);
+        case 4:
+            alterar(lista, nome);
             break;
 
-            break;
-
-        case 5: printf("### LISTAR ####\n"); Contato **contatos = obterArray(lista);
+        case 5:
+            printf("### LISTAR ####\n");
+            Contato **contatos = obterArray(lista);
             for (int i = 0; i < tamanho(lista); i++)
             {
                 printf("**************************\n");
@@ -369,14 +334,24 @@ int main()
             }
             break;
 
-        case 6: printf("\nSaindo da sua agenda de contatos...\n"); exit(1); break;
+        case 6:
+            printf("\nSaindo da sua agenda de contatos...\n");
+                 Contato *contato = obterArray(lista);
 
-        default: printf("\nOpcao invalida!\n"); break;
+                for(int i = 0; i < tamanho(lista); i++){
+
+                    salvarContato(contatos[i]);
+                }
+                exit(1);
+            break;
+    
+        default:
+            printf("\nOpcao invalida!\n");
+            break;
         }
         system("pause");
         system("cls");
-    } 
-    while (opcao != 6);
+    } while (opcao != 6);
 
     return 0;
 }
